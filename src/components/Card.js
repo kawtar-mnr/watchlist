2da36868616744679dc93c2563e98458923e7c0c
kawtar-mnr <73318008+kawtar-mnr@users.noreply.github.com>
2021-02-26 23:31:08 +0100

    Add files via upload

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card=({id,title,description})=>{
 	return(
 
 		<div className='tc bg-light-pink br3 dib pa3 ma2 grow bw2 shadow-5'>
-			<img alt='movie' src={`./img/${id}.jpg`} />
+			<img alt='movie' src={`../img/${id}.jpg`} />
 			<div>
 				<h2>{title}</h2>
 			
